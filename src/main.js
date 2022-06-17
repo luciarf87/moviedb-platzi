9c080e424addd98b09d46b8b486814f171d5665c
Lucia Rodriguez Fauve <luciarf87@gmail.com>
2022-06-16 21:51:59 -0300

    Solución error info duplicada

--- src/main.js
+++ src/main.js
@@ -11,8 +11,8 @@ async function getTrendingMoviesPreview() {
     const { data } = await api ('trending/movie/day');
     const movies = data.results;
     
+    trendingMoviesPreviewList.innerHTML = "";
     movies.forEach(movie => {
-       const trendingMoviesPreviewList = document.querySelector('#trendingPreview .trendingPreview-movieList');
         const movieContainer = document.createElement('div');
         movieContainer.classList.add('movie-container');
 
@@ -30,8 +30,9 @@ async function getCategoriesPreview() {
     const { data } = await api ('genre/movie/list');
     const categories = data.genres;
     
+    categoriesPreviewList.innerHTML = "";
+
     categories.forEach(category => {
-       const categoriesPreviewList = document.querySelector('#categoriesPreview .categoriesPreview-list');
         const categoryContainer = document.createElement('div');
         categoryContainer.classList.add('category-container');
 
