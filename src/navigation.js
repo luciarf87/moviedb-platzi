0990011d897375e8de6520c50b4962767b359e6f
Lucia Rodriguez Fauve <luciarf87@gmail.com>
2022-06-17 19:52:10 -0300

    Agregué JS para filtrar películas por categoría

--- src/navigation.js
+++ src/navigation.js
@@ -106,4 +106,11 @@ function categoriesPage() {
     categoriesPreviewSection.classList.add('inactive');
     genericSection.classList.remove('inactive');
     movieDetailSection.classList.add('inactive');
+
+    const [_, categoryData] =  location.hash.split('='); 
+    const [categoryId, categoryName] =  categoryData.split('-');
+
+    headerCategoryTitle.innerHTML = categoryName;
+
+    getMoviesByCategory(categoryId);
 }
